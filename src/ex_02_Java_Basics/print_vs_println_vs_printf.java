699edfa3d342abc77c89a8f1dfe02a50561f6eba
danush4756-ai <danush4756@gmail.com>
2025-12-17 21:26:55 +0530

    Printf

---
+++ src/ex_02_Java_Basics/print_vs_println_vs_printf.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Basics;
+
+public class print_vs_println_vs_printf {
+    public static void main() {
+        System.out.printf("%d*1=%d\n",9, 9*1);
+        System.out.printf("%d*2=%d\n",9, 9*2);
+        System.out.printf("%d*2=%d\n",9, 9*3);
+        System.out.printf("%d*2=%d\n",9, 9*4);
+        System.out.printf("%d*2=%d\n",9, 9*5);
+        System.out.printf("%d*2=%d\n",9, 9*6);
+        System.out.printf("%d*2=%d\n",9, 9*7);
+        System.out.printf("%d*2=%d\n",9, 9*8);
+        System.out.printf("%d*2=%d\n",9, 9*9);
+        System.out.printf("%d*2=%d\n",9, 9*10);
+    }
+
+}
