3607b06f37aa91d54cb881418e1035f32ff460b6
danush4756-ai <danush4756@gmail.com>
2025-12-24 09:11:17 +0530

    Ternary_Operator task dec 24

---
+++ src/Ternary_Operator/nested_ternary_operator.java
@@ -0,0 +1,13 @@
+package Ternary_Operator;
+
+public class nested_ternary_operator {
+    public static void main(String[] args) {
+        int n1 = 2;
+
+        int n2 = 9;
+
+        int n3 = -11;
+int max = (n1 > n2) ? n1 : (n2 > n3)? n2 : n3;
+        System.out.println("MAX OUT OF THREE: " + max);
+    }
+}
