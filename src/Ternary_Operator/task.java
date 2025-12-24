54c4690d8d611d6b1bca2d2a939b84c9fd83fbfe
danush4756-ai <danush4756@gmail.com>
2025-12-24 08:50:07 +0530

    Ternary_Operator

---
+++ src/Ternary_Operator/task.java
@@ -0,0 +1,9 @@
+package Ternary_Operator;
+
+public class task {
+    public static void main(String[] args) {
+        int marks = 50;
+        String result = (marks > 40) ? "pass" : "fail";
+        System.out.println(result);
+    }
+}
