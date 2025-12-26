5967a408846458e0562a926ef5662fba5e537a7d
danush4756-ai <danush4756@gmail.com>
2025-12-26 19:11:42 +0530

    TASK_27_dec_2025_IF_ELSE

---
+++ src/IF_ELSE/TASK_27_dec_2025.java
@@ -0,0 +1,22 @@
+package IF_ELSE;
+
+import java.util.Scanner;
+
+public class TASK_27_dec_2025 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+        a = sc.nextInt(); b = sc.nextInt(); c = sc.nextInt();
+        if (a==b && a==c) {
+            System.out.println("equilateral triangle");
+        } else if (a==b || a==c ||c==b)  {
+            System.out.println("isosceles triangle");
+        }else {
+            System.out.println("scalene triangle");
+
+    }
+        sc.close();
+    }
+
+}
+
