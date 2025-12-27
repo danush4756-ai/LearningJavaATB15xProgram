f717ba216b59aeb42e14fcc9993c351b477f8bc1
danush4756-ai <danush4756@gmail.com>
2025-12-27 21:52:29 +0530

    Interview_concat_plus

---
+++ src/Operators/Interview_concat_plus.java
@@ -0,0 +1,23 @@
+package Operators;
+
+public class Interview_concat_plus {
+    static void main(String[] args) {
+        // +
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+
+        // + -> String -> Concatenation
+        String first_name = "Pramod";
+        String last_name = "Dutta";
+        System.out.println(first_name+last_name); // concatenation
+
+//        System.out.println(first_name-last_name); error
+//        System.out.println(first_name*last_name); error
+//        System.out.println(first_name/last_name); error
+
+        // + - behave arithmetic int
+        // + - concat String
+
+    }
+}
