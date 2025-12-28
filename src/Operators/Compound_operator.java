068d214141724e86af9e348390e2e7437c11503a
danush4756-ai <danush4756@gmail.com>
2025-12-28 18:55:10 +0530

    Compound_operator

---
+++ src/Operators/Compound_operator.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class Compound_operator {
+    static void main(String[] args) {
+        int a =6;
+        a +=9; //6+9=15
+        a -=8;//15-8=7
+        a *=6;//7*6=42
+        a /=2;//42/2=21
+        System.out.println(a);
+    }
+}
