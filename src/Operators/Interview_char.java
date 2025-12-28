58344deba3e760bd471984da052932018a9af719
danush4756-ai <danush4756@gmail.com>
2025-12-28 19:29:12 +0530

    Interview_char

---
+++ src/Operators/Interview_char.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Interview_char {
+    static void main(String[] args) {
+        char a = 'w', b = 'o';
+        System.out.println(a+b);
+        System.out.println('w'+10);
+        System.out.println('w'+'o'==230);
+    }
+}
