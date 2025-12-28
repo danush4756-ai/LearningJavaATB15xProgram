6dac212433a469c21b844ca60f2090764c459a49
danush4756-ai <danush4756@gmail.com>
2025-12-28 19:32:29 +0530

    Interview_char

--- src/Operators/Interview_char.java
+++ src/Operators/Interview_char.java
@@ -6,5 +6,7 @@ public class Interview_char {
         System.out.println(a+b);
         System.out.println('w'+10);
         System.out.println('w'+'o'==230);
+        System.out.println('w'>'o');
+        System.out.println('w'<'o');
     }
 }
