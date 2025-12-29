a5fceb0766455f7fc89692343b2de218a542b92e
danush4756-ai <danush4756@gmail.com>
2025-12-29 19:57:49 +0530

    Interview_increment_decrement

---
+++ src/increment_decrement/Interview_increment_decerement.java
@@ -0,0 +1,10 @@
+package increment_decrement;
+
+public class Interview_increment_decerement {
+    public static void main(String[] args) {
+        int x = 5; int y = ++x;//x value becomes 6 as incremented 1st
+        System.out.println(x + " " + y);
+        int a = x++;
+        System.out.println(x+" "+a);
+    }
+}
