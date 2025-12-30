c8c8585477983b2574f83c743a6b4b1f95ca916d
danush4756-ai <danush4756@gmail.com>
2025-12-30 22:11:16 +0530

    same_value_many_variables

---
+++ src/increment_decrement/same_value_many_variables.java
@@ -0,0 +1,22 @@
+package increment_decrement;
+
+public class same_value_many_variables {
+    public static void main(String[] args) {
+       int a,b,c,f;
+       a=b=c=f=8;
+       int d= ++a;
+       int e= b++;
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(e);
+        System.out.println(c);
+    /*here value of a and b has increased but
+    it will not increase the value of c*/
+        //here the words in next line is not considered as comment because // can be used only for single line
+        Here many words are in red but some are not in red and some are highlighted and some are in orange
+                we will know why this happens in upcoming chapters
+    }
+}
+
+
