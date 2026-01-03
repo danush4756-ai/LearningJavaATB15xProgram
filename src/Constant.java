c949e77c8db8662c96fa2540f96c6d8b539bfd2a
danush4756-ai <danush4756@gmail.com>
2026-01-03 11:52:08 +0530

    Constant

---
+++ src/Constant.java
@@ -0,0 +1,8 @@
+public class Constant {
+    public static void main(String[] args) {
+        int a =20;
+        a = 78; // There is no error because it is not a constant
+        final int age = 100;
+        // age = 45; // this is an error because age is constant now
+    }
+}
