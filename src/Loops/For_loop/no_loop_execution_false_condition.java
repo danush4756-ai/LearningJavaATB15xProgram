c28010f5e233a79b104562f89b4d29e6a1d7030b
danush4756-ai <danush4756@gmail.com>
2026-01-05 09:14:19 +0530

    false condition

---
+++ src/Loops/For_loop/no_loop_execution_false_condition.java
@@ -0,0 +1,10 @@
+package Loops.For_loop;
+
+public class no_loop_execution_false_condition {
+    public static void main(String[] args) {
+        for (int i = 0; i > 1 ; i++) {
+            System.out.println(i);
+        }
+        System.out.println("End");
+    }
+}
