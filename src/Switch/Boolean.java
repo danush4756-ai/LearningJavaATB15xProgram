b3ea5433d09e04211d0e8bae757cd77a9b0b334d
danush4756-ai <danush4756@gmail.com>
2026-01-05 14:34:49 +0530

    boolean cannot be used with switch

--- src/Switch/Boolean.java
+++ src/Switch/Boolean.java
@@ -3,7 +3,7 @@ package Switch;
 public class Boolean {
     public static void main(String[] args) {
         boolean b = true;
-//        switch (b) { //boolen cannot be used with switch
+//        switch (b) { //boolean cannot be used with switch
 //        }
     }
 
