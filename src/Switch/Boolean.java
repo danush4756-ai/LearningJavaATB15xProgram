d9299123d054f834f2309df291696e88fd2fc5b4
danush4756-ai <danush4756@gmail.com>
2026-01-05 14:33:53 +0530

    boolen cannot be used with switch

---
+++ src/Switch/Boolean.java
@@ -0,0 +1,11 @@
+package Switch;
+
+public class Boolean {
+    public static void main(String[] args) {
+        boolean b = true;
+//        switch (b) { //boolen cannot be used with switch
+//        }
+    }
+
+}
+
