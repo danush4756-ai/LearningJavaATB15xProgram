74e2e267d376f625637cbf30d7a9e7c1b19abc7e
danush4756-ai <danush4756@gmail.com>
2026-01-05 12:12:22 +0530

    check user input before user enters the input

---
+++ src/Switch/with_scanner.java
@@ -0,0 +1,42 @@
+package Switch;
+
+import java.util.Scanner;
+
+public class with_scanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number from 1 to 7 to tell which day: ");
+
+        if (scanner.hasNextInt()) { /* this has to be written before asking input because if this is asked
+        after asking user input user input will be executed*/
+            int day = scanner.nextInt();//instead of int if user enter string InputMismatchException error
+            switch (day) {
+                case 1:
+                    System.out.println("Mon");
+                    break;//if break is not added every condition will start executing
+                case 2:
+                    System.out.println("Tue");
+                    break;
+                case 3:
+                    System.out.println("wed");
+                    break;
+                case 4:
+                    System.out.println("thu");
+                    break;
+                case 5:
+                    System.out.println("fri");
+                    break;
+                case 6:
+                    System.out.println("sat");
+                    break;
+                case 7:
+                    System.out.println("sun");
+                    break;
+                default:
+                    System.out.println("Enter number from 1 to 7");
+            }
+        } else {
+            System.out.println("Invalid input");
+        }
+        System.out.println("end of the program");
+    }}
