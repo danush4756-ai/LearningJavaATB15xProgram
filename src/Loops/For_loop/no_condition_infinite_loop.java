150dd4c12e1c79de8df9f874ce4216795e740264
danush4756-ai <danush4756@gmail.com>
2026-01-05 09:19:08 +0530

    exit code 130

---
+++ src/Loops/For_loop/no_condition_infinite_loop.java
@@ -0,0 +1,12 @@
+package Loops.For_loop;
+
+public class no_condition_infinite_loop {
+    public static void main(String[] args) {
+        for (int i = 0; ; i++) {
+            System.out.println(i);
+            // exit code 130 , (Stop by Yourself- Kill) Red Button - interrupted by signal
+        }
+        // exit code 0 - Successful
+    }
+}
+
