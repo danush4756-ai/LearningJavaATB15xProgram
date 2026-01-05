c49f305e6417e74a02e7da36c5e8d0d93e35c7b6
danush4756-ai <danush4756@gmail.com>
2026-01-05 14:47:30 +0530

    type casting interview

---
+++ src/Switch/Type_casting_interview.java
@@ -0,0 +1,9 @@
+package Switch;
+
+public class Type_casting_interview {
+    public static void main(String[] args) {
+        long a11 = 30l;
+        switch ((int) a11) { //this is explicit narrowing
+        }
+    }
+}
