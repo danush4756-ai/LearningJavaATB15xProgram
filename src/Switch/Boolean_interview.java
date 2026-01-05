8d0ff35746fe1f839c21a3516642396305397d52
danush4756-ai <danush4756@gmail.com>
2026-01-05 14:45:14 +0530

    boolean cannot be used with switch

--- src/Switch/Boolean_interview.java
+++ src/Switch/Boolean_interview.java
@@ -1,6 +1,6 @@
 package Switch;
 
-public class Boolean {
+public class Boolean_interview {
     public static void main(String[] args) {
         boolean b = true;
 //        switch (b) { //boolean cannot be used with switch
