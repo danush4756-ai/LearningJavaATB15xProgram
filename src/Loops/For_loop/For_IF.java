a162475db2941bb0cef337c49030565ec1ae79ea
danush4756-ai <danush4756@gmail.com>
2026-01-05 09:39:37 +0530

    for if

---
+++ src/Loops/For_loop/For_IF.java
@@ -0,0 +1,12 @@
+package Loops.For_loop;
+
+public class For_IF {
+    public static void main(String[] args) {
+        for (int tejas = 0; tejas < 18; tejas++) {
+            if( tejas > 15){
+                System.out.println("age: "+tejas+ " Gift from Papa, Iphone 99");
+            }else{
+                System.out.println("age: "+tejas+ " No Gift, you are a KID");
+            }
+    }
+}}
