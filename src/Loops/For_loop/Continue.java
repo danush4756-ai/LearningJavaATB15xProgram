303bd120e70e6c39557a0e85d7e63a206a37b4e7
danush4756-ai <danush4756@gmail.com>
2026-01-08 06:59:57 +0530

    how continue works

---
+++ src/Loops/For_loop/Continue.java
@@ -0,0 +1,17 @@
+package Loops.For_loop;
+
+import java.util.Scanner;
+
+public class Continue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a no.");
+        int num = scanner.nextInt();
+        for (int i = 0; i < num; i++) {
+            if (i == 5) {
+                continue;//when i = 5. it goes back to start next iteration instead of going to next line
+            }
+            System.out.println(i);
+        }
+    }
+}
