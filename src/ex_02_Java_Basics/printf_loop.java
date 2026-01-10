423c8fe9729a6cbbb75f091052057fea5078f233
danush4756-ai <danush4756@gmail.com>
2026-01-10 16:37:19 +0530

    printf loop

---
+++ src/ex_02_Java_Basics/printf_loop.java
@@ -0,0 +1,8 @@
+package ex_02_Java_Basics;
+
+public class printf_loop {
+    public static void main(String[] args) {
+for (int i = 1; i<=10; i++)
+    System.out.printf("%d*%s=%d\n",9,i,i*9);//%d=int (decimal) %s=string
+    }
+}
