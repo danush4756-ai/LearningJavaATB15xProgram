d497b88ed66e1d543eaf45fba44c43ce5cb2c488
danush4756-ai <danush4756@gmail.com>
2026-01-10 20:20:04 +0530

    score & grade

---
+++ src/Ternary_Operator/score_grade.java
@@ -0,0 +1,16 @@
+package Ternary_Operator;
+
+import java.util.Scanner;
+
+public class score_grade {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        String result = (score>100 || score<1) ? "invalid input"
+        : (score>=90 && score<=100) ? "A"
+                : (score>=80 && score<=89) ? "B"
+                : (score>=70 && score<=79) ? "C"
+                : (score>60 && score<=69) ? "D" : "F";
+        System.out.println(result);
+    }
+}
