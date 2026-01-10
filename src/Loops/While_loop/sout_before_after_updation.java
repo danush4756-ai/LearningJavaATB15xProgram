8cb3c67a1bcf453178575c56f978383921cb669c
danush4756-ai <danush4756@gmail.com>
2026-01-10 12:22:45 +0530

    sout before and after updation

--- src/Loops/While_loop/sout_before_after_updation.java
+++ src/Loops/While_loop/sout_before_after_updation.java
@@ -4,13 +4,14 @@ public class sout_before_after_updation {
     public static void main(String[] args) {
         int i = 0; // Init
         while (i < 10) {//condition
-            System.out.println(i);//prints till 0 to 9 because it works like post increment
+            System.out.print(i+" ");//prints till 0 to 9 because it works like post increment
             ++i; // Updation
         }
+        System.out.println(" ");
         int j = 0; // Init
         while (j < 10) {//condition
             j++; // Updation
-            System.out.println(j);//prints 1 to 10 because it works like pre increment
+            System.out.print(j+ " ");//prints 1 to 10 because it works like pre increment
         }
     }
 }
