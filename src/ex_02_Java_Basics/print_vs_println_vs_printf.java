54e05276e498f973d36d893e7422784ae939f153
danush4756-ai <danush4756@gmail.com>
2026-01-10 16:24:40 +0530

    printf

--- src/ex_02_Java_Basics/print_vs_println_vs_printf.java
+++ src/ex_02_Java_Basics/print_vs_println_vs_printf.java
@@ -4,14 +4,14 @@ public class print_vs_println_vs_printf {
     public static void main(String[] args) {
         System.out.printf("%d*1=%d\n",9, 9*1);
         System.out.printf("%d*2=%d\n",9, 9*2);
-        System.out.printf("%d*2=%d\n",9, 9*3);
-        System.out.printf("%d*2=%d\n",9, 9*4);
-        System.out.printf("%d*2=%d\n",9, 9*5);
-        System.out.printf("%d*2=%d\n",9, 9*6);
-        System.out.printf("%d*2=%d\n",9, 9*7);
-        System.out.printf("%d*2=%d\n",9, 9*8);
-        System.out.printf("%d*2=%d\n",9, 9*9);
-        System.out.printf("%d*2=%d\n",9, 9*10);
+        System.out.printf("%d*3=%d\n",9, 9*3);
+        System.out.printf("%d*4=%d\n",9, 9*4);
+        System.out.printf("%d*5=%d\n",9, 9*5);
+        System.out.printf("%d*6=%d\n",9, 9*6);
+        System.out.printf("%d*7=%d\n",9, 9*7);
+        System.out.printf("%d*8=%d\n",9, 9*8);
+        System.out.printf("%d*9=%d\n",9, 9*9);
+        System.out.printf("%d*10=%d\n",9, 9*10);
     }
 
 }
