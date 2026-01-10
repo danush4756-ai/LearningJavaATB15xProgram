aec6097d92f9a1bd8a631b86a1f4ec7b68be8f61
danush4756-ai <danush4756@gmail.com>
2026-01-10 11:29:57 +0530

    print vs println

---
+++ src/ex_01_Java_Basics/print_vs_println.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class print_vs_println {
+    static void main() {
+        System.out.print("hello");
+        System.out.println("darshan"); /*println means print + line. it prints and adds a line so output
+        is in one line in this case*/
+        System.out.print("hello darshan");//line was already added in previous statement so the output is in new line
+    }
+}
