8fa65c1cfb186b276c36a1f8d4679782f9d7aad8
danush4756-ai <danush4756@gmail.com>
2026-01-10 10:13:35 +0530

    Infinite loop

---
+++ src/Loops/While_loop/Boolean.java
@@ -0,0 +1,9 @@
+package Loops.While_loop;
+
+public class Boolean {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Hello, Drashan");
+        }
+    }
+}
