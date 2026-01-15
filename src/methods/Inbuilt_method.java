08495eb2b434609a201d20caf8621bc18dc7b2c6
danush4756-ai <danush4756@gmail.com>
2026-01-15 21:31:48 +0530

    inbuilt function

---
+++ src/methods/Inbuilt_method.java
@@ -0,0 +1,8 @@
+package methods;
+
+public class Inbuilt_method {
+    public static void main(String[] args) {
+        int max = Math.max(3,4);//this is one of the example inbuilt functions
+        System.out.println(max);
+    }
+}
