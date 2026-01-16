5735352e02802870dc2a19b7b9c90b6e3f4670fb
danush4756-ai <danush4756@gmail.com>
2026-01-16 22:01:15 +0530

    reverse string

---
+++ src/string/reverse.java
@@ -0,0 +1,13 @@
+package string;
+import java.util.Scanner;
+public class reverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        String reverse = "";
+        for (int i = input.length()-1; i>=0; i--){
+            reverse = reverse+input.charAt(i);
+        }
+        System.out.println(reverse);
+    }
+}
