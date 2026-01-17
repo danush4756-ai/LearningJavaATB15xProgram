9be8493e043e85df661130f84b822a45b96e5ec2
danush4756-ai <danush4756@gmail.com>
2026-01-17 13:20:02 +0530

    Creating_methods

--- src/methods/Creating_method.java
+++ src/methods/Creating_method.java
@@ -9,4 +9,18 @@ public class Creating_method {
     public static int sum(int a, int b) {
         return a + b;
     }
+    static void non_return_function() {
+        System.out.println("Print something no return or return type");
+    }
+    static int return_int() {
+        System.out.println("Print something no return or return type");
+        return 10;
+    }
+    static boolean return_boolean() {
+        System.out.println("Print something no return or return type");
+        return true;
+    }
+    static float return_float_pi_value(){
+        return 3.14f;
+    }
 }
