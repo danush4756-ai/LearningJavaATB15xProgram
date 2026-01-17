df3a3a4c175bfc7fe4de373e9f4f982d4ac9d400
danush4756-ai <danush4756@gmail.com>
2026-01-17 12:10:43 +0530

    Creating_method

---
+++ src/methods/Creating_method.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class Creating_method {
+    public static void main(String[] args) {
+        //   public static int sum(int a, int b) {
+        //     return a + b;
+        //we cannot create method inside a method
+    }
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
