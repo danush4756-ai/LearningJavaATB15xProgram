1de3588f01cbf84ad5de47dee9ae472d327b8f96
danush4756-ai <danush4756@gmail.com>
2026-01-17 08:48:39 +0530

    Interview

---
+++ src/methods/Interview.java
@@ -0,0 +1,19 @@
+package methods;
+
+public class Interview {
+    public static void main(String[] args) {
+    }
+   // public static void main(String[] args) {
+    //} //there cannot be duplicate signature
+    public static void main(String args) {
+    }
+    public static int main(int args) {
+        return 10;
+    }
+    public static String main1(String args) {
+        return "10";
+    }
+    public static boolean main(boolean args) {
+        return true;
+    }
+}
