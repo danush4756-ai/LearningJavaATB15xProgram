9ed82510bc41dfbded67d5cb10d96256b068d992
danush4756-ai <danush4756@gmail.com>
2026-01-17 17:56:10 +0530

    char

---
+++ src/Operators/Char.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Char {
+    public static void main(String[] args) {
+        char a, b;
+        a = 'A'; b = 'B';
+        System.out.println(a+b);
+        System.out.println((char)(a+b));//to print char instead of a number
+    }
+}
