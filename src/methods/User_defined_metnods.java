220d43e618cbc812d8e4f8361b08089b9070201c
danush4756-ai <danush4756@gmail.com>
2026-01-17 07:20:29 +0530

    User defined methods

---
+++ src/methods/User_defined_metnods.java
@@ -0,0 +1,32 @@
+package methods;
+
+public class User_defined_metnods {
+    public static void main(String[] args) {
+
+        // User Defined Functions.
+
+        // 1.Without Parameters and Without Return Type
+        // 2.Without Parameters but With Return Type
+        // 3.With Parameters and Without Return Type
+        // 4.With Parameters and With Return Type
+
+        // 1.Without Argument / Parameters and Without Return Type.
+        wop_wor_greet();
+        //  2. Without Parameters but With Return Type
+        String msg = wop_wr_greet_2();
+        //  3. With Parameters and Without Return Type ( 90%)
+        greet_with_details("Pramod", 65, 100);
+        //  4. With Parameters and With Return Type
+        int sum = sum_of_two_numbers(3, 4);
+    }
+    static void wop_wor_greet() {
+    }
+    static String wop_wr_greet_2() {
+        return "pramod";
+    }
+    static void greet_with_details(String pramod, int i, int i1) {
+    }
+    static int sum_of_two_numbers(int i, int i1) {
+        return i + i1;
+    }
+    }
