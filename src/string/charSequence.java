425d0ab7333e1cae5ff7c17949749db8bc9fc7a3
danush4756-ai <danush4756@gmail.com>
2026-01-18 11:19:48 +0530

    charSequence and subSequence

--- src/string/charSequence.java
+++ src/string/charSequence.java
@@ -5,5 +5,7 @@ public class charSequence {
         CharSequence s = "Pramod";
         System.out.println(s);
         System.out.println(s.subSequence(1,4));
+        String s1 = "Darshan T S";
+        System.out.println(s1.subSequence(1,4));
     }
 }
