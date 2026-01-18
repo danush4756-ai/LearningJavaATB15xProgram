b24a5668971c79e569b0b30711ef1dd444016914
danush4756-ai <danush4756@gmail.com>
2026-01-18 17:13:10 +0530

    Default values

---
+++ src/Arrays/Default_values.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class Default_values {
+    public static void main(String[] args) {
+        char [] alpha = new char [3];
+        System.out.println(alpha[0]);
+        int [] check = new int[3];
+        System.out.println(check[1]);
+        String [] dam = new String[3];
+        System.out.println(dam[2]);
+    }
+}
