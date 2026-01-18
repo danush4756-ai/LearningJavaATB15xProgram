bce36ac0370f255d5278887414c64772f40a2a2e
danush4756-ai <danush4756@gmail.com>
2026-01-18 11:16:04 +0530

    charSequence and subSequence

---
+++ src/string/charSequence.java
@@ -0,0 +1,9 @@
+package string;
+
+public class charSequence {
+    public static void main(String[] args) {
+        CharSequence s = "Pramod";
+        System.out.println(s);
+        System.out.println(s.subSequence(1,4));
+    }
+}
