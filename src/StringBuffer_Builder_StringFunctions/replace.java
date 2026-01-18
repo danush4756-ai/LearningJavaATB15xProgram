2fac45b8cdd0367236f4a983b64dab398c8727f3
danush4756-ai <danush4756@gmail.com>
2026-01-18 16:27:22 +0530

    replace

---
+++ src/StringBuffer_Builder_StringFunctions/replace.java
@@ -0,0 +1,12 @@
+package StringBuffer_Builder_StringFunctions;
+
+public class replace {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Java");
+        sb.append(" Programming");
+        System.out.println(sb);
+
+        sb.replace(0, 4, "C++");
+        System.out.println(sb);
+    }
+}
