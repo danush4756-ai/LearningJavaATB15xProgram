0b024c861b6c231d091d4ee69a550cb29e3f2486
danush4756-ai <danush4756@gmail.com>
2026-01-18 16:02:35 +0530

    append

---
+++ src/StringBuffer_Builder_StringFunctions/append.java
@@ -0,0 +1,9 @@
+package StringBuffer_Builder_StringFunctions;
+
+public class append {
+    public static void main(String[] args) {
+        StringBuffer stringBuffer = new StringBuffer("Pramod");
+        stringBuffer.append("Dutta");
+        System.out.println(stringBuffer);
+    }
+}
