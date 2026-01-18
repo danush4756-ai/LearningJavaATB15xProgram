999968c7641a6e639179baddcd781c7fd06e385c
danush4756-ai <danush4756@gmail.com>
2026-01-18 11:31:57 +0530

    subString and methods 2

--- src/string/subString.java
+++ src/string/subString.java
@@ -4,5 +4,7 @@ public class subString {
     public static void main(String[] args) {
         String s = "Java".substring(2);
         System.out.println(s);
+        System.out.println("Darshan\nT S".lines().count());
+        System.out.println(s.repeat(3));
     }
 }
