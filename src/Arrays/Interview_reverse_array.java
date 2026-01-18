8fdd7d0b43e0da07c581ee2624e613e05e75b8d0
danush4756-ai <danush4756@gmail.com>
2026-01-18 18:55:17 +0530

    Interview_reverse_array

---
+++ src/Arrays/Interview_reverse_array.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Interview_reverse_array {
+    public static void main(String[] args) {
+        String sports[] = {"cricket", "footBall", "basketBall" };
+        for (int i = sports.length-1;i>=0;i--) {
+            System.out.print(sports[i]+" ");
+        }
+    }
+}
