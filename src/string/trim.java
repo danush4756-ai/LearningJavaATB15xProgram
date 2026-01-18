f2320d3cdfe0d05e7adfb078b2d954653f73e6df
danush4756-ai <danush4756@gmail.com>
2026-01-18 15:05:55 +0530

    trim

---
+++ src/string/trim.java
@@ -0,0 +1,9 @@
+package string;
+
+public class trim {
+    public static void main(String[] args) {
+        String s1 = "             Darshan T S";
+        System.out.println(s1);
+        System.out.println(s1.trim());
+    }
+}
