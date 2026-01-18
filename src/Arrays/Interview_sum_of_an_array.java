b018eb80b526e218cba82992ad3fcdf4d9e28838
danush4756-ai <danush4756@gmail.com>
2026-01-18 19:04:27 +0530

    Interview_sum_of_an_array

---
+++ src/Arrays/Interview_sum_of_an_array.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class Interview_sum_of_an_array {
+    public static void main(String[] args) {
+        char z[] = {'a', 'b', 'c'};
+    int sum = 0;
+    for (int i=0; i<z.length; i++) {
+        sum = sum + z[i];
+        }
+        System.out.println(sum);
+    }
+}
