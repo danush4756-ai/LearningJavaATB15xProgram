43e7e02425e7fe28c5b6e96f907455ee4d6c067e
danush4756-ai <danush4756@gmail.com>
2026-01-19 09:23:03 +0530

    2D Arrays

---
+++ src/Arrays/arrays_2D.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class arrays_2D {
+    public static void main(String[] args) {
+        // 2D
+        int[][] array_2d_old = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    }
+}
