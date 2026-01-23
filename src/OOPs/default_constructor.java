5236e6d722594464de003fe52d25c23de826ce18
danush4756-ai <danush4756@gmail.com>
2026-01-23 21:38:41 +0530

    default Constructor

---
+++ src/OOPs/default_constructor.java
@@ -0,0 +1,25 @@
+package OOPs;
+
+public class default_constructor {
+    public static void main(String[] args) {
+        //Compiler-provided default constructor
+        class Car {
+            int speed;
+            //This is what the compiler internally creates (you don’t see it):
+         /*   class Car {
+                Car() {
+                   //  default constructor */
+                }
+                //User-defined default constructor
+        class bike {
+            int speed;
+            bike() {   // default / no-arg constructor
+                speed = 60;
+            }
+        }
+
+    }
+
+        }
+
+
