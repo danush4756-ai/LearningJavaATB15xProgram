bb9e14db8f270fcdbc4b3376360d6ae5aa1237db
danush4756-ai <danush4756@gmail.com>
2026-01-24 11:52:40 +0530

    constructor overloading

--- src/OOPs/Constructor_overloading.java
+++ src/OOPs/Constructor_overloading.java
@@ -4,7 +4,8 @@ public class Constructor_overloading {
     public static void main(String[] args) {
         Baby b1 = new Baby();
         Baby b2 = new Baby("Pramod");
-        Baby b3 = new Baby("Pramod","10K");
+        Baby b3 = new Baby("10K");
+        Baby b4 = new Baby(5);
        // System.out.println(b3.money); this is not possible we can accesses only through instance variable
     }
 }
@@ -16,7 +17,8 @@ class Baby{
     Baby(String name){
         System.out.println("PC - name");
     }
-    Baby(String name,String money){
+    // Baby(String money){ this gives error because it is duplicate data type from previous constructor
+    Baby (int age) {
         System.out.println("PC - name.age");
     }
 }
