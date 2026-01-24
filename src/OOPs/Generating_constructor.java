35f55cbee77fb36fde4df14a9e203127c58465af
danush4756-ai <danush4756@gmail.com>
2026-01-24 12:55:47 +0530

    Generating_constructor

---
+++ src/OOPs/Generating_constructor.java
@@ -0,0 +1,11 @@
+package OOPs;
+
+public class Generating_constructor {
+    String email, password, submitButton;
+//right click -> select Generate -> create Constructor
+    public Generating_constructor(String email, String password, String submitButton) {
+        this.email = email;
+        this.password = password;
+        this.submitButton = submitButton;
+    }
+}
