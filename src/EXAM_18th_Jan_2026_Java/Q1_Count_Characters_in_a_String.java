30771b764312cfbf63b74378a71bfebc77ef88ad
danush4756-ai <danush4756@gmail.com>
2026-01-27 22:30:27 +0530

    Exam 18th jan 2026

--- src/EXAM_18th_Jan_2026_Java/Q1_Count_Characters_in_a_String.java
+++ src/EXAM_18th_Jan_2026_Java/Q1_Count_Characters_in_a_String.java
@@ -1,7 +1,16 @@
 package EXAM_18th_Jan_2026_Java;
-
+import java.util.Scanner;
 public class Q1_Count_Characters_in_a_String {
     public static void main(String[] args) {
-        String s1 = "Darshan T S";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a word: ");
+        String word = scanner.nextLine();
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != ' ') {  // Ignore spaces
+                count++;
+            }
+        }
+            System.out.println("Number of letters: " + count);
+        }
     }
-}
