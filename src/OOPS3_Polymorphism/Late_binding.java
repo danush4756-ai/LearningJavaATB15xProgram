f1b2c822181da98ccfd4ecfcc7d5b4d608c8cc79
danush4756-ai <danush4756@gmail.com>
2026-01-28 20:20:53 +0530

    Late binding

---
+++ src/OOPS3_Polymorphism/Late_binding.java
@@ -0,0 +1,22 @@
+package OOPS3_Polymorphism;
+public class Late_binding {
+    public static void main(String[] args) {
+        // Even without "polymorphic" intent, this uses late binding
+        Animal a = new Dog();
+        a.sound();  // LATE BINDING - JVM checks actual object type at runtime
+        //late binding is not possible without method overriding
+    }
+}
+    class Animal {
+        void sound() {
+            System.out.println("Some sound");
+        }
+    }
+
+    class Dog extends Animal {
+        void sound() {
+            System.out.println("Bark");
+        }
+    }
+
+
