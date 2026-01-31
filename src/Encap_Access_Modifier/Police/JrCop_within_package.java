70a0a0f1696f533f90a06938fac4466ed4f0e2ef
danush4756-ai <danush4756@gmail.com>
2026-01-31 14:02:51 +0530

    Access modifiers within package

--- src/Encap_Access_Modifier/Police/JrCop_within_package.java
+++ src/Encap_Access_Modifier/Police/JrCop_within_package.java
@@ -1,10 +1,10 @@
 package Encap_Access_Modifier.Police;
 
-public class JrCop {
+public class JrCop_within_package {
      /*jrcop.CanIShoot(); Executable statements must always be inside methods, constructors or
      initialization blocks - never directly at the class level.*/
     public static void main(String[] args) {
-        Cop jrcop = new Cop(8); //public
+        Cop_within_class jrcop = new Cop_within_class(8); //public
         jrcop.CanIShoot();//protected
 jrcop.UnderstandingDefault();//accessing default within same package
        // jrcop.IdCard = "Darshan"; private access allows access only within class
