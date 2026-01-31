11fa9d3bf03cdd930ae2d34f35a73328486158a3
danush4756-ai <danush4756@gmail.com>
2026-01-31 15:18:36 +0530

    subclass through inheritance

---
+++ src/Encap_Access_Modifier/Criminal/JrThief_subclass_through_inheritance.java
@@ -0,0 +1,10 @@
+package Encap_Access_Modifier.Criminal;
+import Encap_Access_Modifier.Police.Cop_within_class;
+public class JrThief_subclass_through_inheritance extends Cop_within_class {
+    public JrThief_subclass_through_inheritance(int bullet) {
+        super(bullet);
+        Cop_within_class clear = new JrThief_subclass_through_inheritance(56); //public access
+        super.CanIShoot(); /*If protected members are in different packages, you can only access them through
+        super or a reference of the subclass type. So clear.CanIShoot; is not allowed*/
+    }
+}
