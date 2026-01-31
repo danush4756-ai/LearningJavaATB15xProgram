7dc0ac16d0d5631feb0535ecbac1e86460fe9544
danush4756-ai <danush4756@gmail.com>
2026-01-31 14:04:22 +0530

    Access modifiers in different package

--- src/Encap_Access_Modifier/Criminal/Thief_in_different_package.java
+++ src/Encap_Access_Modifier/Criminal/Thief_in_different_package.java
@@ -1,8 +1,8 @@
 package Encap_Access_Modifier.Criminal;
-import Encap_Access_Modifier.Police.Cop; //importing user created class
-public class Thief {
+import Encap_Access_Modifier.Police.Cop_within_class; //importing user created class
+public class Thief_in_different_package {
     public static void main(String[] args) {
-Cop crime = new Cop(13); // public access modifier can be assessed even from different package
+Cop_within_class crime = new Cop_within_class(13); // public access modifier can be assessed even from different package
 /*crime.CanIShoot(); protected cannot be accessed from different package unless it is subclassed through
 inheritance*/
 //crime.IdCard = "Check"; private accessed outside the package
