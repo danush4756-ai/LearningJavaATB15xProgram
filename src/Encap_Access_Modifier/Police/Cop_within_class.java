9eb490f16ca81b61352212fe841187a3a453c81e
danush4756-ai <danush4756@gmail.com>
2026-01-31 14:01:55 +0530

    Access modifiers within class

--- src/Encap_Access_Modifier/Police/Cop_within_class.java
+++ src/Encap_Access_Modifier/Police/Cop_within_class.java
@@ -1,18 +1,18 @@
 package Encap_Access_Modifier.Police;
 
-public class Cop {
+public class Cop_within_class {
     private String IdCard = "1st cop";
     private int gun;
     public static void main() {
        /* private String IdCard; ❌ ERROR: private not allowed inside a method. Access Modifiers
         cannot be used for Local variables*/
-        Cop WithinClass = new Cop(100);
+        Cop_within_class WithinClass = new Cop_within_class(100);
         WithinClass.IdCard = "1st cop"; //private access within class
         WithinClass.gun = 56; //public access within class
         WithinClass.CanIShoot();//protected access within class
         WithinClass.UnderstandingDefault();//Default (no modifier) access within class
     }
-        public Cop(int bullet) {
+        public Cop_within_class(int bullet) {
         this.gun = bullet;
         }
         protected void CanIShoot () {
