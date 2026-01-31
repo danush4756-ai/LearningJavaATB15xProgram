98034b74b9c462bc16c4c880dc7f9ee8245cf99b
danush4756-ai <danush4756@gmail.com>
2026-01-31 12:21:15 +0530

    Access modifiers outside the package.

---
+++ src/Encap_Access_Modifier/Criminal/Thief.java
@@ -0,0 +1,12 @@
+package Encap_Access_Modifier.Criminal;
+import Encap_Access_Modifier.Police.Cop; //importing user created class
+public class Thief {
+    public static void main(String[] args) {
+Cop crime = new Cop(13); // public access modifier can be assessed even from different package
+/*crime.CanIShoot(); protected cannot be accessed from different package unless it is subclass through
+inheritance*/
+//crime.IdCard = "Check"; private accessed outside the package
+       // crime.UnderstandingDefault(); defaut cannot be accessed outside the package
+    }
+    }
+
