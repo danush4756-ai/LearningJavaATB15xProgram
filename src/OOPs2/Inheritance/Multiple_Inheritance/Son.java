f1b0e1dc0b3d3ada7c1592e68ea0653595d13694
danush4756-ai <danush4756@gmail.com>
2026-02-01 08:05:38 +0530

    Multiple Inheritance

--- src/OOPs2/Inheritance/Multiple_Inheritance/Son.java
+++ src/OOPs2/Inheritance/Multiple_Inheritance/Son.java
@@ -1,5 +1,5 @@
 package OOPs2.Inheritance.Multiple_Inheritance;
 
-public class Son // extends Father1, Father2 { class cannot extend multiple calsses
+public class Son // extends Father1, Father2 { class cannot extend multiple classes
 {}
 
