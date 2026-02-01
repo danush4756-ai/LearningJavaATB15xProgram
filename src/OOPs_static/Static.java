7116c2f21dc04a921e62cf785ed80e3f2a518c0a
danush4756-ai <danush4756@gmail.com>
2026-02-01 19:55:48 +0530

    Static

--- src/OOPs_static/Static.java
+++ src/OOPs_static/Static.java
@@ -6,17 +6,22 @@ public class Static {
     int nonstatic = 10;
     static String STATIC = "Darshan";
     static {
-        System.out.println("hello");
+        System.out.println("Static Block"); //static block
     }
     public static void main(String[] args) {
         int a = 10;
         Static check = new Static();
-        System.out.println(check.nonstatic);
-        check.nonStaticMethod();
-      //System.out.println(nonstatic); Non-static field cannot be referenced from a static context
+        System.out.println(check.nonstatic); //uses Object Access
+        check.nonStaticMethod(); //calls an instance method through the object reference.
+        Static.STATIC = "Static Variable"; //static variable
         System.out.println(STATIC);
+        Static.StaticMethod(); //static method
+      //System.out.println(nonstatic); Non-static field cannot be referenced from a static context
     }
     void nonStaticMethod () {
         System.out.println(nonstatic);
+    }
+    static void StaticMethod () {
+        System.out.println("Static method");
     }
 }
