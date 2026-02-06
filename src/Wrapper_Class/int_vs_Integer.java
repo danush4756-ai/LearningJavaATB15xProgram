9c78a851dcc7bfadae70d1a2b5e606576929f3ac
danush4756-ai <danush4756@gmail.com>
2026-02-06 06:30:55 +0530

    int vs Integer

---
+++ src/Wrapper_Class/int_vs_Integer.java
@@ -0,0 +1,14 @@
+package Wrapper_Class;
+
+public class int_vs_Integer {
+    public static void main(String[] args) {
+        int a = 10;
+        //        a. // no attribute
+//        a.len() // no method
+        System.out.println(a);
+        Integer a1 = 10;
+        System.out.println(a1);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+    }
+}
