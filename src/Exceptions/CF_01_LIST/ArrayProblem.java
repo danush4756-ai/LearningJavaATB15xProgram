16897bbd3db1d0138923829a8af0471a91b8bb9c
danush4756-ai <danush4756@gmail.com>
2026-02-07 15:14:52 +0530

    Array problem

---
+++ src/Exceptions/CF_01_LIST/ArrayProblem.java
@@ -0,0 +1,15 @@
+package Exceptions.CF_01_LIST;
+import java.util.ArrayList;
+import java.util.List;
+public class ArrayProblem {
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[10];
+        arr[0]= 1;
+        // arr[0]= "Pramod";
+        // 1. Fixed Size
+        // 2. Same data can  be stored.
+        // 3. wastage of memory for the 9 elements
+        // 4. insertion and deletion is heavy operation.
+        List arrayList = new ArrayList();
+    }
+}
