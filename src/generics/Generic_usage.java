856d1f3b08a0693b65bf04ee8ad3fd6cb0efe7b6
danush4756-ai <danush4756@gmail.com>
2026-02-07 20:18:08 +0530

    Generics usage

---
+++ src/generics/Generic_usage.java
@@ -0,0 +1,11 @@
+package generics;
+import java.util.ArrayList;
+import java.util.List;
+public class Generic_usage {
+    public static void main(String[] args) {
+        List <String> myList = new ArrayList();
+        myList.add("pramod");
+        myList.add("Dutta");
+      //  myList.add(123);
+    }
+}
