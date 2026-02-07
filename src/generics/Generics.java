dc47291b1e823ddccfa749e42469120a4232b4ef
danush4756-ai <danush4756@gmail.com>
2026-02-07 18:41:30 +0530

    Generics

---
+++ src/generics/Generics.java
@@ -0,0 +1,12 @@
+package generics;
+
+public class Generics {
+    public static void main(String[] args) {
+        temp(3, 4);
+        temp(3.34, 4.45);
+        temp("pramod", "dutta");
+    }
+    static <darsh> darsh temp(darsh a, darsh b) {
+        return null;
+    }
+}
