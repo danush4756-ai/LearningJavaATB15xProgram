2cf1a50931e9d91c80cb0220116a3f4621c45021
danush4756-ai <danush4756@gmail.com>
2026-02-07 21:15:47 +0530

    ArrayList_Capacity_Expansion

---
+++ src/Exceptions/CF_01_LIST/ArrayList_Capacity_Expansion.java
@@ -0,0 +1,24 @@
+package Exceptions.CF_01_LIST;
+import java.util.ArrayList;
+import java.util.List;
+public class ArrayList_Capacity_Expansion {
+    public static void main(String[] args) {
+        List check = new ArrayList();
+        check.add("1");//1
+        check.add("1");//2
+        check.add("1");//3
+        check.add("1");//4
+        check.add("1");//5
+        check.add("1");//6
+        check.add("1");//7
+        check.add("1");//8
+        check.add("1");//9
+        check.add("1");//10 after this if I add elements it will increase the capacity to 15 (+5)
+        check.add("1");//11
+        check.add("1");//12
+        check.add("1");//13
+        check.add("1");//14
+        check.add("1");//15 after this if I add elements it will increase the capacity to 22 (+7)
+        check.add("1");//16
+    }
+}
