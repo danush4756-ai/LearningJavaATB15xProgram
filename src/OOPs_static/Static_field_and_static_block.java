2d5a9d67b4429f3b8172c21015360ea7e0e3b629
danush4756-ai <danush4756@gmail.com>
2026-02-07 14:34:10 +0530

    Static field and static block

--- src/OOPs_static/Static_field_and_static_block.java
+++ src/OOPs_static/Static_field_and_static_block.java
@@ -18,7 +18,7 @@ public class Static_field_and_static_block {
      public static void main(String[] args) {
          System.out.println("main");
          test();
-       //  test1(); Non-static method cannot be referanced from a static context
+       //  test1(); Non-static method cannot be referenced from a static context
          Static_field_and_static_block check = new Static_field_and_static_block();
          check.test1();
      }
