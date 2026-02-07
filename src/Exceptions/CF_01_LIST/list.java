3c9f9e8327ebdc9a1a273f10f65727776bb954cf
danush4756-ai <danush4756@gmail.com>
2026-02-07 17:45:30 +0530

    list

---
+++ src/Exceptions/CF_01_LIST/list.java
@@ -0,0 +1,15 @@
+package Exceptions.CF_01_LIST;
+import java.util.ArrayList;
+import java.util.List;
+public class list {
+    public static void main(String[] args) {
+        List fruits = List.of("orange","apple","guava","mango","watermelon");
+        System.out.println(fruits);
+//        List mylist = new List();
+        List arrayList = new ArrayList();
+        arrayList.add("Pramod");
+        arrayList.add(123);
+        arrayList.add(true);
+        System.out.println(arrayList);
+    }
+}
