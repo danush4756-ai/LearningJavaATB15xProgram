d7e2e65ea87fdd2dacf557257e0d738f266b8239
danush4756-ai <danush4756@gmail.com>
2026-02-08 08:24:49 +0530

    Setting_Initial_Size

---
+++ src/Exceptions/CF_01_LIST/Setting_Initial_Size.java
@@ -0,0 +1,20 @@
+package Exceptions.CF_01_LIST;
+import java.util.*;
+public class Setting_Initial_Size {
+    public static void main(String[] args) {
+        // List s = new List()
+        List mylist0 = new ArrayList();
+        List mylist1 = new ArrayList(5);
+        System.out.println(2131);//1
+        System.out.println(2131);//2
+        System.out.println(2131);//3
+        System.out.println(2131);//4
+        System.out.println(2131);//5
+        System.out.println(2131);//6 at 6th add, capacity becomes 7
+        System.out.println(2131);//7
+        System.out.println(2131);//8 capacity becomes 10
+        System.out.println(2131);//9
+        System.out.println(2131);//10
+        System.out.println(2131);//11
+    }
+}
