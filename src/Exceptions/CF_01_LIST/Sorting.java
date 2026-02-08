4786ab05dd43663742f9c9675175bb357f5309fe
danush4756-ai <danush4756@gmail.com>
2026-02-08 10:47:58 +0530

    Sorting

---
+++ src/Exceptions/CF_01_LIST/Sorting.java
@@ -0,0 +1,16 @@
+package Exceptions.CF_01_LIST;
+import java.util.*;
+public class Sorting {
+    public static void main(String[] args) {
+        List marks = new ArrayList();
+        marks.add(91);
+        marks.add(95);
+        marks.add(56);
+        marks.add(89);
+        System.out.println(marks); //original
+        Collections.sort(marks);
+        System.out.println(marks);//ascending
+        Collections.sort(marks, Collections.reverseOrder());
+        System.out.println(marks);//descending
+    }
+}
