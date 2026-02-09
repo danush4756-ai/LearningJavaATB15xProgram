b9688198238a2085a34dd136ec5b0a6755fbe4f2
danush4756-ai <danush4756@gmail.com>
2026-02-09 18:44:42 +0530

    ArrayProblem

--- src/Exceptions/CF_01_LIST/ArrayProblem.java
+++ src/Exceptions/CF_01_LIST/ArrayProblem.java
@@ -3,13 +3,18 @@ import java.util.ArrayList;
 import java.util.List;
 public class ArrayProblem {
     public static void main(String[] args) {
-        Integer[] arr = new Integer[10];
+        Integer[] arr = new Integer[3];
         arr[0]= 1;
-        // arr[0]= "Pramod";
-        // 1. Fixed Size
-        // 2. Same data can  be stored.
-        // 3. wastage of memory for the 9 elements
-        // 4. insertion and deletion is heavy operation.
+        arr[1]= 1;
+        //arr[2]= 'x'; Homogeneous Data Only
+        //No Dynamic Memory Allocation
+        //No Ready Sorting
+        for (Integer i : arr) {
+            System.out.print(i + " ");
+        }
+        //arr[2]= 3; out of bound (Fixed Size)
+        //System.out.println(arr.size()); no methods
+        //insertion and deletion is heavy operation.
         List arrayList = new ArrayList();
     }
 }
