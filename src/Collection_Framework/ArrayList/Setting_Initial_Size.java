9f526956b5ba1b9ff821740efb8c3881f926ac61
danush4756-ai <danush4756@gmail.com>
2026-02-11 19:47:28 +0530

    Setting initial size

--- src/Collection_Framework/ArrayList/Setting_Initial_Size.java
+++ src/Collection_Framework/ArrayList/Setting_Initial_Size.java
@@ -5,16 +5,17 @@ public class Setting_Initial_Size {
         // List s = new List()
         List mylist0 = new ArrayList();
         List mylist1 = new ArrayList(5);
-        System.out.println(2131);//1
-        System.out.println(2131);//2
-        System.out.println(2131);//3
-        System.out.println(2131);//4
-        System.out.println(2131);//5
-        System.out.println(2131);//6 at 6th add, capacity becomes 7
-        System.out.println(2131);//7
-        System.out.println(2131);//8 capacity becomes 10
-        System.out.println(2131);//9
-        System.out.println(2131);//10
-        System.out.println(2131);//11
+        mylist1.add(2131);//1
+        mylist1.add(2131);//2
+        mylist1.add(2131);//3
+        mylist1.add(2131);//4
+        mylist1.add(2131);//5
+        mylist1.add(2131);//6 at 6th add, capacity becomes 7
+        mylist1.add(2131);//7
+        mylist1.add(2131);//8 capacity becomes 11
+        mylist1.add(2131);//9
+        mylist1.add(2131);//10
+        mylist1.add(2131);//11
+        System.out.println(mylist1);
     }
 }
