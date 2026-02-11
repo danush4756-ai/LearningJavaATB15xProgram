7a01dced31480e54a588dde370c50f6162483054
danush4756-ai <danush4756@gmail.com>
2026-02-11 20:53:23 +0530

    list

--- src/Collection_Framework/ArrayList/list.java
+++ src/Collection_Framework/ArrayList/list.java
@@ -4,6 +4,7 @@ import java.util.List;
 public class list {
     public static void main(String[] args) {
         List fruits = List.of("orange","apple","guava","mango","watermelon");
+        //fruits.add("kiwi"); Unsupported Operation Exception
         System.out.println(fruits);
       //  List mylist = new List(); List is abstract
         List arrayList = new ArrayList();
