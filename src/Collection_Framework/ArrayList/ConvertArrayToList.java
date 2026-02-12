865e6893007f54cf56cc5208628aa422fe0adc2b
danush4756-ai <danush4756@gmail.com>
2026-02-12 06:46:23 +0530

    Convert Array to List

--- src/Collection_Framework/ArrayList/ConvertArrayToList.java
+++ src/Collection_Framework/ArrayList/ConvertArrayToList.java
@@ -5,7 +5,7 @@ public class ConvertArrayToList {
         String arr[] = {"Java","Python","C#"};
         System.out.println(Arrays.stream(arr).count()); /*stream() is a static method of the Arrays utility class
         but Arrays don't have class*/
-        //arr.set(2, "c++"); methods not allowed in arrays
+        //arr.set(2, "c++"); arrays don't have methods
         List<String> list = Arrays.asList(arr);
         list.set(2, "c++");
         /*list.add("Darshan"); Unsupported Operation Exception. Arrays.asList(arr) does NOT return
