2bc941292cbcb09b24e074ba3fa5ee8ea19a9fe5
danush4756-ai <danush4756@gmail.com>
2026-02-14 18:32:55 +0530

    HashMap, LinkedHashMap, and TreeMap

--- src/Collection_Framework/MAP/HashMap_LinkedHashMap_TreeMap.java
+++ src/Collection_Framework/MAP/HashMap_LinkedHashMap_TreeMap.java
@@ -6,16 +6,19 @@ public class HashMap_LinkedHashMap_TreeMap {
         m1.put("name", "pramod");
         m1.put("rollno", 1);
         m1.put("phone", 987654321);
+        m1.put(null,null);
         System.out.println(m1);
         Map m2 = new LinkedHashMap();
         m2.put("name","pramod");
         m2.put("rollno",1);
         m2.put("phone",987654321);
+        m2.put(null,null);
         System.out.println(m2);
         Map m3 = new TreeMap();
         m3.put("name","pramod");
         m3.put("rollno",1);
         m3.put("phone",987654321);
+       // m3.put(null,null); Null Pointer Exception
         System.out.println(m3);
   }
 }
