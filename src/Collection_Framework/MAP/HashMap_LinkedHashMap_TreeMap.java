74e1f3b690730fda1174d48504e9e32eeb1b503a
danush4756-ai <danush4756@gmail.com>
2026-02-14 17:50:27 +0530

    MAP

---
+++ src/Collection_Framework/MAP/HashMap_LinkedHashMap_TreeMap.java
@@ -0,0 +1,21 @@
+package Collection_Framework.MAP;
+import java.util.*;
+public class HashMap_LinkedHashMap_TreeMap {
+    public static void main(String[] args) {
+        Map m1 = new HashMap();
+        m1.put("name", "pramod");
+        m1.put("rollno", 1);
+        m1.put("phone", 987654321);
+        System.out.println(m1);
+        Map m2 = new LinkedHashMap();
+        m2.put("name","pramod");
+        m2.put("rollno",1);
+        m2.put("phone",987654321);
+        System.out.println(m2);
+        Map m3 = new TreeMap();
+        m3.put("name","pramod");
+        m3.put("rollno",1);
+        m3.put("phone",987654321);
+        System.out.println(m3);
+  }
+}
