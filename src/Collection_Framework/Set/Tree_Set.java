c933e4b45cf923d61a40fa07521599065a489682
danush4756-ai <danush4756@gmail.com>
2026-02-14 12:13:55 +0530

    TreeSet

---
+++ src/Collection_Framework/Set/Tree_Set.java
@@ -0,0 +1,17 @@
+package Collection_Framework.Set;
+import java.util.*;
+public class Tree_Set {
+    public static void main() {
+        Set ts = new TreeSet();
+        // Black and Red Tree mechanism to store the element.
+        // Natural Sorting order is maintained.
+        ts.add("Dapple");
+        ts.add("Apple");
+        ts.add("Orange");
+        ts.add("WaterMelon");
+        ts.add("WaterMelon");
+        //ts.add(123); not allowed because it effects sorting
+        //ts.add(null); not allowed because it effects sorting
+        System.out.println(ts);
+   }
+}
