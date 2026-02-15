2cae87126138fd80ea38a59e2c86cf3bcb82eadd
danush4756-ai <danush4756@gmail.com>
2026-02-15 11:29:33 +0530

    class_usage

--- src/generics/class_usage.java
+++ src/generics/class_usage.java
@@ -1,11 +1,14 @@
 package generics;
 import java.util.ArrayList;
 import java.util.List;
-public class class_usage {
+public class class_usage <T> {
+    private T value;
+    public void setValue(T value){
+        this.value = value;
+    }
+    public T getValue(){
+        return value;
+    }
     public static void main(String[] args) {
-        List myList = new ArrayList();
-        myList.add("pramod");
-        myList.add("Dutta");
-        myList.add(123);
     }
 }
