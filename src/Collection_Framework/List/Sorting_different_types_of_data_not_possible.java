c5f0f7341635eeda9a846075d47476cebe280a4b
danush4756-ai <danush4756@gmail.com>
2026-02-16 13:56:27 +0530

    Sorting different types of data

--- src/Collection_Framework/List/Sorting_different_types_of_data_not_possible.java
+++ src/Collection_Framework/List/Sorting_different_types_of_data_not_possible.java
@@ -8,6 +8,10 @@ public class Sorting_different_types_of_data_not_possible {
         marks.add('T');
         marks.add(5.89);
         System.out.println(marks); //original
+        Collections.sort(marks,(a, b) -> a.toString().compareTo(b.toString()));
+        System.out.println(marks);
+        Collections.sort(marks, (b, a) -> a.toString().compareTo(b.toString()));
+        System.out.println(marks);
        /* Collections.sort(marks);
         System.out.println(marks);//ascending
         Collections.sort(marks, Collections.reverseOrder());
