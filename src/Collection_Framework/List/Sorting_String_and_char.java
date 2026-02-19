0476ee4e2a5d7095928db4e49ab17342561cb378
danush4756-ai <danush4756@gmail.com>
2026-02-19 20:59:53 +0530

    Sorting_String_and_char

--- src/Collection_Framework/List/Sorting_String_and_char.java
+++ src/Collection_Framework/List/Sorting_String_and_char.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public class Sorting_String_and_char {
     public static void main(String[] args) {
-        List marks = new ArrayList();
+        //List<char> marks = new ArrayList<>(); Generics do not support primitive data types.
+        List<Character> marks = new ArrayList<>();
         marks.add('1'); //Digits '0'-'9' range from 48 to 57. Value 49
         marks.add('.'); //Special characters usually come before numbers and letters.
         marks.add('7'); //Value 55
@@ -17,7 +18,7 @@ public class Sorting_String_and_char {
         Collections.sort(marks, Collections.reverseOrder());
         System.out.println(marks);//descending
         System.out.println("-----");
-        List marks1 = new ArrayList();
+        List<String> marks1 = new ArrayList<>();
         marks1.add("Darshan"); //'D' (from "Darshan") has an ASCII value of 68.
         marks1.add("T S"); //'T' (from "T S") has an ASCII value of 84.
         System.out.println(marks1); //original
