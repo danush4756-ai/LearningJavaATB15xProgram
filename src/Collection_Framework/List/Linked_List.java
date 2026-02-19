2239671dbe59fed56b4957780f30e8b9010ee64b
danush4756-ai <danush4756@gmail.com>
2026-02-19 21:13:58 +0530

    Linked_List

--- src/Collection_Framework/List/Linked_List.java
+++ src/Collection_Framework/List/Linked_List.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Linked_List {
     public static void main(String[] args) {
 //        List my_list = new ArrayList();
-        List mylist = new LinkedList();
+        List<Integer> mylist = new LinkedList<>();
         mylist.add(1);
         mylist.add(2);
         mylist.add(3);
@@ -18,7 +18,8 @@ public class Linked_List {
         System.out.println("4 "+mylist);
         mylist.remove(2);
         System.out.println("5 "+mylist);
-        List mylist2 = new ArrayList();
+        System.out.println("--------");
+        List<String> mylist2 = new ArrayList<>();
         mylist2.addLast("Darshan");
         mylist2.addFirst("T S");
         System.out.println("6 "+mylist2);
